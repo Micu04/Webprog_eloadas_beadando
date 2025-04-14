f473d6aeabcfbd728bc0c5716c612633e036fe82
Micu04 <jmiklos04@gmail.com>
2025-04-14 16:09:27 +0200

    2 feladat alap struktura

---
+++ 2_feladat/SPA/src/pages/Emojenius.jsx
@@ -0,0 +1,6 @@
+function Emojenius(){
+    return(
+<p>Think you speak fluent emoji? Put your skills to the test in Emojenius – the game where pictures say it all!</p>
+    );
+}
+export default Emojenius
