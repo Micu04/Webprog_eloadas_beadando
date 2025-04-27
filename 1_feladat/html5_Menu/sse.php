19b7c53145dd56117d0f1b6c5125f9a4893fac4e
Pinkhey <pinkasztamas03@gmail.com>
2025-04-27 15:56:12 +0200

    utolsó finomítások

--- 1_feladat/html5_Menu/sse.php
+++ 1_feladat/html5_Menu/sse.php
@@ -1,8 +1,11 @@
-<?php  
-    header('Content-Type: text/event-stream');
-    header('Cache-Control: no-cache');
-    
-    $time = date('r');
-    echo "data: The server time is: {$time}\n\n";
+<?php
+header('Content-Type: text/event-stream');
+header('Cache-Control: no-cache');
+
+while (true) {
+    echo "data: Szervernél a jelenlegi idő: " . date("H:i:s") . "\n\n";
+    ob_flush();
     flush();
-?>  
+    sleep(1);
+}
+?>
